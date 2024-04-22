06eefbcf544bed0847ad292b4c003c274d592fe0
NoretsAndrii <147941173+NoretsAndrii@users.noreply.github.com>
2024-04-23 00:48:22 +0300

    add profile css

--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -1,32 +1,37 @@
-const Product = ({ name, tag, location, image, stats }) => {
-  return (
-    <div>
-  <div>
-    <img
-      src={image}
-      alt={name}
-    />
-    <p>{name}</p>
-    <p>@{tag}</p>
-    <p>{location}</p>
-  </div>
+import './Profile.css';
 
-  <ul>
-    <li>
-      <span>Followers</span>
-      <span>{stats.followers}</span>
-    </li>
-    <li>
-      <span>Views</span>
-      <span>{stats.views}</span>
-    </li>
-    <li>
-      <span>Likes</span>
-      <span>{stats.likes}</span>
-    </li>
-  </ul>
-</div>
+const Profile = ({ name, tag, location, image, stats }) => {
+  return (
+    <div className="profile-card">
+      <div className="profile-card-main">
+        <img className="profile-card-main-img" src={image} alt={name} />
+        <p className="profile-card-main-name">{name}</p>
+        <p className="profile-card-main-text">@{tag}</p>
+        <p className="profile-card-main-text">{location}</p>
+      </div>
+
+      <ul className="profile-card-info-list">
+        <li className="profile-card-info-list-item">
+          <span className="profile-card-info-list-item-title">Followers</span>
+          <span className="profile-card-info-list-item-count">
+            {stats.followers}
+          </span>
+        </li>
+        <li className="profile-card-info-list-item">
+          <span className="profile-card-info-list-item-title">Views</span>
+          <span className="profile-card-info-list-item-count">
+            {stats.views}
+          </span>
+        </li>
+        <li className="profile-card-info-list-item">
+          <span className="profile-card-info-list-item-title">Likes</span>
+          <span className="profile-card-info-list-item-count">
+            {stats.likes}
+          </span>
+        </li>
+      </ul>
+    </div>
   );
 };
 
-export default Product;
+export default Profile;
